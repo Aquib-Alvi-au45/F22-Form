577d38e8124bbefe5319df880f1550dff808c6d7
Aquib Alvi <aquibalvi831@gmail.com>
2022-10-16 17:50:41 +0530

    feat : added form validation using formik and yup

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,45 +1,140 @@
 import React from "react";
-import classes from './Forms.module.css'
-import Input from "./Input";
+import { ErrorMessage, Formik } from "formik";
+import * as Yup from "yup";
+import classes from "./Forms.module.css";
+
+const initialValue = {
+  email: "",
+  companyName: "",
+  password: "",
+  acceptTerms: false,
+};
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email()
+    .required("Email is required")
+    .email("Email is invalid"),
+
+  companyName: Yup.string()
+    .max(30, "Too Long!")
+    .required("Company Name is required"),
+
+  password: Yup.string()
+    .required("Password is required")
+    .min(8, "Password must be at least 8 characters")
+    .max(20, "Password must not exceed 20 characters"),
+
+  acceptTerms: Yup.boolean().oneOf(
+    [true],
+    "Accept Terms & Conditions is required"
+  ),
+});
 
 const Forms = () => {
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
 
-
-
-    return (
-        <div className={classes.Rectangle}>
-
-            <div className={classes.main}>
-                <span className={classes.SignUp}>
-                    Sign Up
+  return (
+    <Formik
+      initialValues={initialValue}
+      validationSchema={schema}
+      onSubmit={handleSubmit}>
+      {(formik) => {
+        return (
+          <form onSubmit={formik.handleSubmit}>
+            <div className={classes.Rectangle}>
+              <div className={classes.main}>
+                <span className={classes.SignUp}>Sign Up</span>
+                <span className={classes["No-credit-card-requi"]}>
+                  No credit card required
                 </span>
-                <span class={classes['No-credit-card-requi']}>
-                    No credit card required
-                </span>
-                <Input type='email' placeholder='Email address' name='email' />
-                <Input type='email' placeholder='Email address' name='email' />
-                <Input type='email' placeholder='Email address' name='email' />
-            </div>
-            <input className={classes.Path} type='checkbox' />
-            <span class={classes["I-agree-to-the-Terms"]}>
-                <span class={classes["text-style-1"]}>I agree to the</span>Terms & Conditions
-            </span>
 
-            <div className={classes.main}>
-                <button className={classes["btn-get"]}>Get Started</button>
-                <div>
+                <div className={classes.formDiv}>
+                  <input
+                    className={classes.inputBox}
+                    type="email"
+                    placeholder="Email address"
+                    name="email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                  />
 
-                    <span class={classes["Already-have-an-acco"]}>
-                        Already have an account?<span class={classes["text-style-1"]}>Sign in</span>
-                    </span>
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className={classes.formErrorMsg}
+                  />
                 </div>
+
+                <div className={classes.formDiv}>
+                  <input
+                    className={classes.inputBox}
+                    type="text"
+                    placeholder="Company name"
+                    name="companyName"
+                    value={formik.values.companyName}
+                    onChange={formik.handleChange}
+                  />
+                  <ErrorMessage
+                    name="companyName"
+                    component="div"
+                    className={classes.formErrorMsg}
+                  />
+                </div>
+
+                <div className={classes.formDiv}>
+                  <input
+                    className={classes.inputBox}
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                  />
+                  <ErrorMessage
+                    name="password"
+                    component="div"
+                    className={classes.formErrorMsg}
+                  />
+                </div>
+              </div>
+              <input
+                className={classes.Path}
+                type="checkbox"
+                name="acceptTerms"
+                value={formik.values.acceptTerms}
+                onChange={formik.handleChange}
+              />
+
+              <span className={classes["I-agree-to-the-Terms"]}>
+                <span className={classes["text-style-1"]}>I agree to the</span>
+                Terms & Conditions
+              </span>
+              <ErrorMessage
+                name="acceptTerms"
+                component="div"
+                className={classes.formErrorMsg}
+              />
+
+              <div className={classes.main}>
+                <button className={classes["btn-get"]} type="submit">
+                  Get Started
+                </button>
+                <div>
+                  <span className={classes["Already-have-an-acco"]}>
+                    Already have an account?
+                    <span className={classes["text-style-1"]}>Sign in</span>
+                  </span>
+                </div>
+              </div>
             </div>
-        </div>
+          </form>
+        );
+      }}
+    </Formik>
+  );
+};
 
-
-    )
-
-
-}
-
-export default Forms
+export default Forms;
